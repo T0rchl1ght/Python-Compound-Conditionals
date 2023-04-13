3c8cede337b25523a571af236d6b5c6f3ae4801f
T0rchl1ght <122705226+T0rchl1ght@users.noreply.github.com>
2023-04-12 20:42:20 -0700

    Add files via upload

---
+++ compound_conditionals.py
@@ -0,0 +1,9 @@
+give_me_money = True
+fuel_my_car = True
+
+if give_me_money and fuel_my_car:
+    print('No you are too generous')
+elif give_me_money or fuel_my_car:
+    print('Thank you')
+else:
+    print('I need your help!')
